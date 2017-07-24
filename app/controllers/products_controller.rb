ddc2dd5bf90aba21c04f1e3c4a8fc6b0cb706c0c
Jay Deverett <jayjdeverett@gmail.com>
2017-07-24 18:56:39 -0400

    add all views, forms, activerecord methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,54 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new
+
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      flash.now[:notice] = 'Product created successfully!'
+      redirect_to '/products'
+
+    else
+      flash.now[:notice] = 'Sorry. That did not work.'
+      render :new
+    end
+
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  def update
+    @product = Product.find(params[:id])
+      if @product.update(product_params)
+        redirect_to @product
+      else
+        redirect_back_or_to @product
+      end
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def show
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
   end
 end
